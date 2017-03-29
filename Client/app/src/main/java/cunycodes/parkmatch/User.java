21a75deb16b9e914de368f6414d804ba5ea7cb4c
Amena Khan <amenaikhn@yahoo.com>
2017-03-29 19:46:50 -0400

    Fixed Build errors for time picker

--- Client/app/src/main/java/cunycodes/parkmatch/User.java
+++ Client/app/src/main/java/cunycodes/parkmatch/User.java
@@ -6,7 +6,7 @@ package cunycodes.parkmatch;
 public class User {
     private String name, userName, email, id;
 
-    public User(String name, String userName, String email, String id) {
+    public User() {
 
     }
 
