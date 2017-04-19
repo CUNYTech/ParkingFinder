bfb98ef5f96cb7d516d8a5d5846d05791bc2c7d5
Jws57 <JAMES.SANTANA@baruchmail.cuny.edu>
2017-04-18 20:07:39 -0400

    temporarily remove notification feature

--- Client/app/src/main/java/cunycodes/parkmatch/AlarmService.java
+++ Client/app/src/main/java/cunycodes/parkmatch/AlarmService.java
@@ -33,11 +33,16 @@ public class AlarmService extends IntentService {
                 new Intent(this, MapsActivity.class), 0);
 
         NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
-                this).setContentTitle("Alarm").setSmallIcon(R.drawable.icon)
+                this).setContentTitle("Alarm")
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
                 .setContentText(msg);
 
-
+        /*
+        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.icon)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+         */
         alamNotificationBuilder.setContentIntent(contentIntent);
         alarmNotificationManager.notify(1, alamNotificationBuilder.build());
         Log.d("AlarmService", "Notification sent.");
