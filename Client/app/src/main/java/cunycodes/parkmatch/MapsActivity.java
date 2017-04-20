77f28e5fd0f333525b7a16f1fc6bb0e3e4ef1a22
Amena Khan <amenaikhn@yahoo.com>
2017-04-19 21:50:08 -0400

    replaced corrupted image for alarm

--- Client/app/src/main/java/cunycodes/parkmatch/MapsActivity.java
+++ Client/app/src/main/java/cunycodes/parkmatch/MapsActivity.java
@@ -352,7 +352,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             alarmMinute =minLeaving;
 
 
-            //((MapsActivity)getActivity()).ActivateAlarm();
+            ((MapsActivity)getActivity()).ActivateAlarm();
 
             String timeLeaving = Integer.toString(hourOfDay) + ":" + Integer.toString(minute);
             wdata.writeToDatabase (newLong, newLat, hourLeaving, minLeaving);
