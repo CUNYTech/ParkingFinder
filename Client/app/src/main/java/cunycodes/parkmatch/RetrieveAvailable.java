63e2bebe41106f3313f11cb8d2733d205c66a981
Amena Khan <amenaikhn@yahoo.com>
2017-04-22 21:32:41 -0400

    comments removed

--- Client/app/src/main/java/cunycodes/parkmatch/RetrieveAvailable.java
+++ Client/app/src/main/java/cunycodes/parkmatch/RetrieveAvailable.java
@@ -29,7 +29,7 @@ public class RetrieveAvailable {
     private HashMap<String, GeoLocation> availableSpots;
     private int totalAvailable;
     AvailableSpot selected;
-    final private double searchRadius = 3.2; //sarch radius is in Km
+    final private double searchRadius = 3.2; //search radius is in Km
 
     public RetrieveAvailable (GeoLocation requested) {
         this.requested = requested;
